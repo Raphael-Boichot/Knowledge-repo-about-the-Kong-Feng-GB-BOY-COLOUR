6b9c34a877a477a3d03958b62b5b84108ffdbb40
Raphael-Boichot <63796796+Raphael-Boichot@users.noreply.github.com>
2023-09-13 19:50:04 +0200

    Add files via upload

--- Dump/HITEK_MULTI_finder.m
+++ Dump/HITEK_MULTI_finder.m
@@ -6,7 +6,14 @@ fileID = fopen(name);
 data=fread(fileID);
 k = strfind(data',[0xCE 0xED 0x66 0x66])-261;%Nintendo logo
 
+disp('| Data (rom or junk):                  | Size in bytes:          |Starting offset:             |Starting bank:             |')
+disp('|----------------------------|-------------------------|-----------------------------|-----------------------------|')
+
 for i=1:1:length(k)-1
-    disp(['Rom name: ',char(data(k(i)+309:k(i)+324))',' Size in bytes: 0x',dec2hex(k(i+1)-k(i),6),' - Starting offset: 0x',dec2hex(k(i),6)])
+    disp(['| ',char(data(k(i)+309:k(i)+324))',' | 0x',dec2hex(k(i+1)-k(i),6),' | 0x',dec2hex(k(i),6),'|',dec2hex(k(i)/2^15),'|'])
 end
-disp(['Rom name: ',char(data(k(end)+309:k(end)+324))',' Size in bytes: 0x',dec2hex(length(data)-k(end),6),' - Starting offset: 0x',dec2hex(k(end),6)])
+disp(['| ',char(data(k(end)+309:k(end)+324))',' | 0x',dec2hex(length(data)-k(end),6),' | 0x',dec2hex(k(end),6),'|',dec2hex(k(end)/2^15),'|'])
+
+fid = fopen('HITEK_MULTI_alone.gb','w');
+fwrite(fid,data(1:2^15));
+fclose(fid);
